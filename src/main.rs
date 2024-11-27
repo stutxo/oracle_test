e7cbf3ecda28ea113021cd8cbcb3434da54ead51
stutxo <70952638+stutxo@users.noreply.github.com>
2024-11-27 00:26:56 -0500

    add ctv script

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,23 @@
 use std::{collections::HashMap, vec};
 
 use bitcoin::{
+    consensus::Encodable,
+    hashes::{sha256, Hash},
     key::{Keypair, Secp256k1},
-    opcodes::all::{OP_CHECKSIG, OP_CHECKSIGVERIFY},
+    opcodes::all::{OP_CHECKSIG, OP_CHECKSIGVERIFY, OP_NOP4},
     script::Builder,
-    secp256k1::{self, PublicKey},
-    taproot::{TaprootBuilder, TaprootSpendInfo},
-    Address, ScriptBuf, XOnlyPublicKey,
+    secp256k1::{self, Message, PublicKey},
+    sighash::{Prevouts, SighashCache},
+    taproot::{LeafVersion, TaprootBuilder, TaprootSpendInfo},
+    Address, Amount, ScriptBuf, Sequence, TapLeafHash, TapSighashType, Transaction, TxOut,
+    XOnlyPublicKey,
 };
+
 use rand::{rngs::ThreadRng, SeedableRng};
 use schnorr_fun::{
     adaptor::{Adaptor, EncryptedSign},
-    fun::{g, marker::*, nonce, s, Scalar, G},
-    Message, Schnorr,
+    fun::{g, marker::*, nonce, s, Point, Scalar, G},
+    Schnorr,
 };
 use sha2::{digest::Update, Sha256};
 
@@ -80,7 +85,7 @@ fn main() {
         encryption_keys.insert(outcome.to_string(), encryption_key);
 
         // Create the message for the outcome
-        let message = Message::<Public>::plain("CET", outcome.as_bytes());
+        let message = schnorr_fun::Message::<Public>::plain("CET", outcome.as_bytes());
 
         // Generate the encrypted signature (adaptor signature)
         let encrypted_signature = schnorr.encrypted_sign(&alice_keypair, &encryption_key, message);
@@ -160,8 +165,9 @@ fn main() {
                     .get(outcome)
                     .expect("encrypted signature exists");
 
-                let message = Message::<Public>::plain("CET", outcome.as_bytes());
+                let message = schnorr_fun::Message::<Public>::plain("CET", outcome.as_bytes());
 
+                //do i use this to unlock the oracles script?
                 let decrypted_signature =
                     schnorr.decrypt_signature(*s_oracle_non_zero, encrypted_signature.clone());
 
@@ -174,6 +180,8 @@ fn main() {
                     "Decrypted and verified signature for the actual outcome: {}",
                     outcome
                 );
+
+                //test unlocking the script here
 
                 match schnorr.recover_decryption_key(
                     encryption_key,
@@ -199,21 +207,55 @@ fn create_script(
     bob_pub_key: PublicKey,
     oracle_encryption_key_white: XOnlyPublicKey,
     oracle_encryption_key_black: XOnlyPublicKey,
-) -> Result<(TaprootSpendInfo)> {
+) -> Result<TaprootSpendInfo> {
     println!("🏗️ Creating address for game");
     let secp = Secp256k1::new();
 
     let combined_pubkey = secp256k1::PublicKey::combine_keys(&[&white_pub_key, &bob_pub_key])
         .expect("Failed to combine keys");
 
-    let white_script = dlchess_script_win(
-        XOnlyPublicKey::from_slice(&oracle_encryption_key_white.serialize()).unwrap(),
-        white_pub_key.x_only_public_key().0,
+    let ctv_receive_address_white_win = TaprootBuilder::new()
+        .finalize(&secp, white_pub_key.into())
+        .unwrap();
+
+    let ctv_output_address_white_win = Address::p2tr_tweaked(
+        ctv_receive_address_white_win.output_key(),
+        bitcoin::Network::Signet,
     );
 
-    let black_script = dlchess_script_win(
+    let white_win_outputs = [TxOut {
+        //how to do fees?
+        value: Amount::from_sat(100_000),
+        script_pubkey: ctv_output_address_white_win.script_pubkey(),
+    }];
+
+    let white_win_ctv_hash = calc_ctv_hash(&white_win_outputs);
+
+    let ctv_receive_address_black_win = TaprootBuilder::new()
+        .finalize(&secp, white_pub_key.into())
+        .unwrap();
+
+    let ctv_output_address_black_win = Address::p2tr_tweaked(
+        ctv_receive_address_black_win.output_key(),
+        bitcoin::Network::Signet,
+    );
+
+    let black_win_outputs = [TxOut {
+        //how to do fees?
+        value: Amount::from_sat(100_000),
+        script_pubkey: ctv_output_address_black_win.script_pubkey(),
+    }];
+
+    let black_win_ctv_hash = calc_ctv_hash(&black_win_outputs);
+
+    let white_script = dlchess_script_win_ctv(
+        XOnlyPublicKey::from_slice(&oracle_encryption_key_white.serialize()).unwrap(),
+        white_win_ctv_hash,
+    );
+
+    let black_script = dlchess_script_win_ctv(
         XOnlyPublicKey::from_slice(&oracle_encryption_key_black.serialize()).unwrap(),
-        bob_pub_key.clone().x_only_public_key().0,
+        black_win_ctv_hash,
     );
 
     let taproot_spend_info = TaprootBuilder::new()
@@ -227,11 +269,93 @@ fn create_script(
     Ok(taproot_spend_info)
 }
 
-fn dlchess_script_win(oracle_pubkey: XOnlyPublicKey, player_pubkey: XOnlyPublicKey) -> ScriptBuf {
+// fn dlchess_script_win(oracle_pubkey: XOnlyPublicKey, player_pubkey: XOnlyPublicKey) -> ScriptBuf {
+//     Builder::new()
+//         .push_x_only_key(&oracle_pubkey)
+//         .push_opcode(OP_CHECKSIGVERIFY)
+//         .push_x_only_key(&player_pubkey)
+//         .push_opcode(OP_CHECKSIG)
+//         .into_script()
+// }
+
+fn dlchess_script_win_ctv(oracle_pubkey: XOnlyPublicKey, ctv_hash: [u8; 32]) -> ScriptBuf {
     Builder::new()
+        .push_slice(ctv_hash)
+        .push_opcode(OP_NOP4)
         .push_x_only_key(&oracle_pubkey)
-        .push_opcode(OP_CHECKSIGVERIFY)
-        .push_x_only_key(&player_pubkey)
         .push_opcode(OP_CHECKSIG)
         .into_script()
 }
+
+fn calc_ctv_hash(outputs: &[TxOut]) -> [u8; 32] {
+    let mut buffer = Vec::new();
+    buffer.extend(2_i32.to_le_bytes()); // version
+    buffer.extend(0_i32.to_le_bytes()); // locktime
+    buffer.extend(1_u32.to_le_bytes()); // inupts len
+
+    let seq = sha256::Hash::hash(&Sequence::default().0.to_le_bytes());
+    buffer.extend(seq.to_byte_array()); // sequences
+
+    let outputs_len = outputs.len() as u32;
+    buffer.extend(outputs_len.to_le_bytes()); // outputs len
+
+    let mut output_bytes: Vec<u8> = Vec::new();
+    for o in outputs {
+        o.consensus_encode(&mut output_bytes).unwrap();
+    }
+    buffer.extend(sha256::Hash::hash(&output_bytes).to_byte_array()); // outputs hash
+
+    buffer.extend(0_u32.to_le_bytes()); // inputs index
+
+    let hash = sha256::Hash::hash(&buffer);
+    hash.to_byte_array()
+}
+
+// fn spend_win<'a>(
+//     unsigned_tx: &'a mut Transaction,
+//     prev_tx: Vec<TxOut>,
+//     sighash_type: TapSighashType,
+//     taproot_spend_info: TaprootSpendInfo,
+//     winning_pub_key: &'a Point<Normal>,
+//     winning_player: &'a Keypair,
+//     oracle_winning_decryption_key: Option<Scalar<Secret, NonZero>>,
+// ) -> &'a mut Transaction {
+//     println!("Spending for win");
+//     let secp = Secp256k1::new();
+//     let unsigned_tx_clone = unsigned_tx.clone();
+
+//     let winner_script = dlchess_script_win(
+//         XOnlyPublicKey::from_slice(&winning_pub_key.to_xonly_bytes()).unwrap(),
+//         winning_player.x_only_public_key().0,
+//     );
+//     let tap_leaf_hash = TapLeafHash::from_script(&winner_script, LeafVersion::TapScript);
+//     let winning_priv_key = Keypair::from_secret_key(
+//         &secp,
+//         &secp256k1::SecretKey::from_slice(&oracle_winning_decryption_key.unwrap().to_bytes())
+//             .unwrap(),
+//     );
+
+//     for (index, input) in unsigned_tx.input.iter_mut().enumerate() {
+//         let sighash = SighashCache::new(&unsigned_tx_clone)
+//             .taproot_script_spend_signature_hash(
+//                 index,
+//                 &Prevouts::All(&prev_tx),
+//                 tap_leaf_hash,
+//                 sighash_type,
+//             )
+//             .expect("failed to construct sighash");
+
+//         let message = Message::from(sighash);
+//         let oracle_signature = secp.sign_schnorr_no_aux_rand(&message, &winning_priv_key);
+//         let winning_player_signature = secp.sign_schnorr_no_aux_rand(&message, winning_player);
+
+//         let script_ver = (winner_script.clone(), LeafVersion::TapScript);
+//         let ctrl_block = taproot_spend_info.control_block(&script_ver).unwrap();
+
+//         input.witness.push(winning_player_signature.serialize());
+//         input.witness.push(oracle_signature.serialize());
+//         input.witness.push(script_ver.0.into_bytes());
+//         input.witness.push(ctrl_block.serialize());
+//     }
+//     unsigned_tx
+// }
